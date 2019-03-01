a964132ffdd774e2f1ddc767abf0af7508fb4a78
Eric Goncalves <eric@gnclvs.com>
2019-03-01 09:24:04 -0500

    react siema

--- example/src/App.js
+++ example/src/App.js
@@ -4,9 +4,23 @@ import ExampleComponent from 'react-maisonette-slider'
 
 export default class App extends Component {
   render () {
+    const options = {
+      perPage: {
+        768: 2,
+        1024: 3
+      },
+      multipleDrag: true,
+      loop: true
+    }
+
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ExampleComponent options={options}>
+          <div><img src='https://pawelgrzybek.com/siema/assets/siema--pink.svg' alt='Siema' /></div>
+          <div><img src='https://pawelgrzybek.com/siema/assets/siema--yellow.svg' alt='Siema' /></div>
+          <div><img src='https://pawelgrzybek.com/siema/assets/siema--pink.svg' alt='Siema' /></div>
+          <div><img src='https://pawelgrzybek.com/siema/assets/siema--yellow.svg' alt='Siema' /></div>
+        </ExampleComponent>
       </div>
     )
   }
